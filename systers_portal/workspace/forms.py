d9090129ad7f4eff570d44de98181e541cc68f4a
ana-balica <ana.balica@gmail.com>
2014-03-18 21:43:58 +0200

    save Resource from the ModelForm

--- systers_portal/workspace/forms.py
+++ systers_portal/workspace/forms.py
@@ -5,4 +5,4 @@ from workspace.models import Resource
 class ResourceForm(ModelForm):
     class Meta:
         model = Resource
-        exclude = ['date']
+        exclude = ['date', 'author']
--- systers_portal/workspace/views.py
+++ systers_portal/workspace/views.py
@@ -5,6 +5,7 @@ from django.http.request import QueryDict
 
 from workspace.forms import ResourceForm
 from workspace.models import Resource
+from dashboard.models import User
 
 
 def index(request):
@@ -14,9 +15,9 @@ def index(request):
 def add_resource(request):
     form = ResourceForm()
     if request.method == 'POST':
-        resource_params = request.POST.dict()
-        resource_params[u"author"] = unicode(request.user.id)
-        resource = ResourceForm(resource_params)
+        form = ResourceForm(request.POST)
+        resource = form.save(commit=False)
+        resource.author = User(request.user.id)
         resource.save()
         return redirect(index)
     return render(request, 'add_resource.html', {'form': form})
