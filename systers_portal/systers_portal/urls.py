0de08b952c5f0df3ba29d22e09cf2e629479e739
ana-balica <ana.balica@gmail.com>
2014-03-18 18:40:19 +0200

    index template+view+url path

--- systers_portal/systers_portal/urls.py
+++ systers_portal/systers_portal/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
+from workspace.views import index
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'systers_portal.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', index, name="index"),
+    url(r'^index$', index, name="index"),
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
