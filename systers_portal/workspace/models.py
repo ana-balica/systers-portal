c111cbe35577e3fc020d44f0cc1bfaf15d0e5bb4
ana-balica <ana.balica@gmail.com>
2014-03-18 21:27:35 +0200

    make date from Resource model default to today

--- systers_portal/workspace/models.py
+++ systers_portal/workspace/models.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django.db import models
 from dashboard.models import User
 from django_countries.fields import CountryField
@@ -19,7 +21,7 @@ RESOURCE_STATUS = {
 class Resource(models.Model):
     type = models.CharField(max_length=255, choices=RESOURCE_TYPES)
     author = models.ForeignKey(User, null=False)
-    date = models.DateField()
+    date = models.DateField(default=date.today)
     title = models.CharField(max_length=511, blank=False, null=False)
     status = models.CharField(max_length=31, choices=RESOURCE_STATUS)
     location = CountryField()
