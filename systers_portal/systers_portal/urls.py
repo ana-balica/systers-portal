e894370691cdf0cc3a6aa7f2fc8611f9b82785d6
ana-balica <ana.balica@gmail.com>
2014-03-18 20:39:37 +0200

    add resouce url, template and a view that servers only GET requests

--- systers_portal/systers_portal/urls.py
+++ systers_portal/systers_portal/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from workspace.views import index
+from workspace.views import index, add_resource
 from dashboard.views import login_view, logout_view
 
 admin.autodiscover()
@@ -9,6 +9,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', index, name="index"),
     url(r'^index$', index, name="index"),
+    url(r'^add_resource$', add_resource, name="add_resource"),
     url(r'^login/', login_view, name="login"),
     url(r'^logout/', logout_view, name="logout"),
     url(r'^grappelli/', include('grappelli.urls')),
--- systers_portal/workspace/views.py
+++ systers_portal/workspace/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
-from models import Resourse
+from workspace.forms import ResourceForm
+from workspace.models import Resource
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def add_resource(request):
+    form = ResourceForm()
+    return render(request, 'add_resource.html', {'form': form})
